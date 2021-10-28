385a6bc67cd63ab0fe623b677f572d44841d982a
Ngon Vu <ngonvu.6626@gmail.com>
2021-10-28 13:39:13 +0700

    20.10.2021

--- dashboard-page/assets/scripts/index.js
+++ dashboard-page/assets/scripts/index.js
@@ -1,9 +1,3 @@
-function nocontext(e) {
-  var clickedTag = e == null ? event.srcElement.tagName : e.target.tagName;
-  if (clickedTag == "IMG") return false;
-}
-document.oncontextmenu = nocontext;
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
