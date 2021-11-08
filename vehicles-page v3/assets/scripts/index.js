9527c5e924f49ada53d70a05205b983c8d0eaf99
Ngon Vu <ngonvu.6626@gmail.com>
2021-11-08 13:03:36 +0700

    update 08.11.21

--- vehicles-page v3/assets/scripts/index.js
+++ vehicles-page v3/assets/scripts/index.js
@@ -4,6 +4,11 @@ const $$ = document.querySelectorAll.bind(document);
 // get menuPrimary
 const tabsNav = $$(".js-listItem");
 const panesNav = $$(".contents");
+const modal = $("#modal");
+const btnBack = $("#btn-back");
+const btnClose = $("#btn-close");
+const viewsMore = $$(".js-viewMore");
+const picksColor = $$(".colorItem");
 
 // menu primary
 tabsNav.forEach((tabNav, index) => {
@@ -15,5 +20,29 @@ tabsNav.forEach((tabNav, index) => {
 
     this.classList.add("active");
     paneNav.classList.add("openBlock");
+    modal.classList.remove("openBlock");
+  };
+});
+
+// View More
+viewsMore.forEach((viewMore, index) => {
+  viewMore.onclick = function () {
+    modal.classList.add("openBlock");
+  };
+});
+// View More - btn-back
+btnBack.onclick = function () {
+  modal.classList.remove("openBlock");
+};
+btnClose.onclick = function () {
+  modal.classList.remove("openBlock");
+};
+
+// //Thay doi hieu ung select khi click vào tung the Color
+picksColor.forEach((pickColor, index) => {
+  pickColor.onclick = function () {
+    $(".colorItem.active").classList.remove("active");
+
+    this.classList.add("active");
   };
 });
